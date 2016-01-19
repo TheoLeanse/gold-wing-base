2d34b260a9e9e27c4acf7c622bc1b79fe994df2d
TheoLeanse <theoleanse@gmail.com>
2016-01-19 23:07:14 +0000

    add click to open nav

---
+++ public/main.js
@@ -0,0 +1,6 @@
+const text = document.querySelector(".gw-heading__text");
+const navbar = document.querySelector(".gw-navbar__options");
+
+text.addEventListener("click", () => {
+    navbar.style.display = "inherit";
+});
