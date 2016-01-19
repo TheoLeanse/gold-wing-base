3eae13d6c9e19e58db50a0df46b71a2d6e027cac
TheoLeanse <theoleanse@gmail.com>
2016-01-19 16:19:39 +0000

    favicon

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express	= require('express');
 const path	= require('path');
+const favicon	= require('serve-favicon');
+
 const app	= express();
 
 app.set('port', (process.env.PORT || 3030));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 app.listen(app.get('port'), () => {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
